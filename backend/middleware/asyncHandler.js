2b4dec1961e16e7aa8524c9cb63e0f9f8c83948e
Matias Villar <matiase.villar@gmail.com>
2023-12-09 17:35:06 -0700

    Getting ready to launch server in build mode

--- backend/middleware/asyncHandler.js
+++
@@ -1,4 +0,0 @@
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next); // instead of using a bunch of try catch blocks, we can use this asyncHandler function to catch errors and pass them to the error handler middleware
-
-export default asyncHandler;
